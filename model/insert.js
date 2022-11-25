679244d97bd65cfaa123fcc4127c4cc777084a51
Geovana Hekce <geovana.shecke@gmail.com>
2022-11-24 21:47:16 -0300

    linhas alteradas

--- model/insert.js
+++ model/insert.js
@@ -1,9 +1,8 @@
 let mysql  = require('mysql');
 let connection = require('./con_bd.js');
 
-// insert statment
+
 let sql = `INSERT INTO usuario(login,senha)
            VALUES('sergio', 123456)`;
 
-// execute the insert statment
 connection.query(sql);
