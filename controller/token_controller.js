ef35425e3f30f86c3294010265cf998fc2453f36
picheidt <viniciuspicheidt@gmail.com>
2022-11-29 21:29:07 -0300

    Revert "fix structure"
    This reverts commit 2c22a1dc4ba9ea9e5f69c422319cae7c9266e067.

--- controller/token_controller.js
+++
@@ -1,8 +0,0 @@
-require("dotenv-safe").config();
-const jwt = require('jsonwebtoken');
-
-
-function gerar_token(id){
-    const token = jwt.sign({id}, process.env.SECRET, {expiresIn:})
-    return token
-}
--- routes/routes.js
+++ routes/routes.js
@@ -1,7 +1,28 @@
 const express = require('express')
 const router = express.Router()
+
+require("dotenv-safe").config();
+const jwt = require('jsonwebtoken');
+
 const bodyParser = require('body-parser').urlencoded({extended:false})
-const verifyToken = require('../middleware/middleware')
+
+
+// função que verifica o token de autenticação
+function verifyToken(req, res, next){
+    console.log(req.cookies)
+    const token = req.cookies['x-access-token'];
+    if (!token){
+        return res.status(401).json({ auth: false, message: 'No token provided.' });
+    } 
+
+    jwt.verify(token, process.env.SECRET, function(err, decoded) {
+      if (err){
+        return res.status(500).json({ auth: false, message: 'Failed to authenticate token.' });
+      } 
+      next();
+    });
+}
+
 // rota que retorna a tela de listar
 router.get('/contatos', verifyToken, (req, res)=>{
     res.render('listar.html')
